dbe45eeaaeb3b38a1a81711e98dbf0ac2c7fabee
Carlos Gabriel da Silva Machado <cgdsm.eng18@uea.edu.br>
2021-03-16 23:04:05 -0400

    fazendo alguns testes com enumerate.

--- Data-Science-do-Zero/teste.py
+++ Data-Science-do-Zero/teste.py
@@ -1,4 +1,15 @@
 lista = [1, 2, 7, 3, 4, 5]
 
-lista = sorted(lista)
-print(lista)
+# sorted tem mais parametros, pesquisar.
+lista = sorted(lista)  # orderna a lista
+
+"""
+# enumerate é uma tupla com dois valores onde o
+# primeiro valor é a posição do objeto e o segundo
+# valor é a posição i do objeto
+exemplo abaixo
+"""
+
+for i, j in enumerate(lista):
+    print(f"Valor de i: {i}")  # posição na lista
+    print(f"Valor de j: {j}")  # valor referente a posição na lista
