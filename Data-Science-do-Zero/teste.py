7e9a6eb434d6372739aae1c41434d97beb4711bb
Carlos Gabriel da Silva Machado <cgdsm.eng18@uea.edu.br>
2021-03-28 14:18:36 -0400

    Foi adicinado uma usuabilidade do enumerate no python.

--- Data-Science-do-Zero/teste.py
+++ Data-Science-do-Zero/teste.py
@@ -7,9 +7,15 @@ lista = sorted(lista)  # orderna a lista
 # enumerate é uma tupla com dois valores onde o
 # primeiro valor é a posição do objeto e o segundo
 # valor é a posição i do objeto
-exemplo abaixo
+# exemplo abaixo
 """
 
 for i, j in enumerate(lista):
     print(f"Valor de i: {i}")  # posição na lista
     print(f"Valor de j: {j}")  # valor referente a posição na lista
+
+
+"""
+# exemplo com geradores
+# geradores são uma forma de criar loops mais eficientes
+"""
